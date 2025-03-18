a57ecc0541a35342e13ef3e378171548a4086603
tharunteja18 <105804653+tharunteja18@users.noreply.github.com>
2025-03-18 23:08:18 +0530

    Delete src directory

--- src/main/java/com/nerchuko/quizapp/Persistance/QuizDao.java
+++
@@ -1,7 +0,0 @@
-package com.nerchuko.quizapp.Persistance;
-
-import com.nerchuko.quizapp.Model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizDao extends JpaRepository<Quiz, Integer> {
-}
